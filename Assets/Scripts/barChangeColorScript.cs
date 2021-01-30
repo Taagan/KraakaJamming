0efb3c17ef31101cecf024b18ddb30df9f993843
toapapper <62546257+toapapper@users.noreply.github.com>
2021-01-30 19:22:25 +0100

    combat_o_lite_annat

---
+++ Assets/Scripts/barChangeColorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barChangeColorScript : MonoBehaviour
+{
+
+    public Color color1 = Color.red;
+    public Color color2 = Color.green;
+
+    public bool color2Active = false;
+
+    private Image bar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (color2Active)
+            bar.color = color2;
+        else
+            bar.color = color1;
+    }
+}
