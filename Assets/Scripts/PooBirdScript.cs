550bbf61b79d7e1312604bbba8b6bd429732d4a0
toapapper <62546257+toapapper@users.noreply.github.com>
2021-01-31 11:52:33 +0100

    aaa

--- Assets/Scripts/PooBirdScript.cs
+++ Assets/Scripts/PooBirdScript.cs
@@ -18,7 +18,7 @@ public class PooBirdScript : Enemy
 
     public override void Attack()
     {
-        Instantiate(poop, new Vector3(this.transform.position.x, this.transform.position.y - 1), Quaternion.identity);
+        Instantiate(poop, new Vector3(this.transform.position.x, this.transform.position.y - 1, -5), Quaternion.identity);
     }
 
     public override void Start()
@@ -95,7 +95,11 @@ public class PooBirdScript : Enemy
     {
         if (currentTarget != null)
         {
-            this.transform.position += this.transform.right * speed * Time.deltaTime;
+            //this.transform.position += this.transform.right * speed * Time.deltaTime;
+            if(facingRight)
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            else
+                transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
 
             if (Vector3.Distance(this.transform.position, currentTarget.transform.position) > ignoreDistance)
             {
